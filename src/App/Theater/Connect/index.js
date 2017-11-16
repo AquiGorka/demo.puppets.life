e3276c623cb9480495afa67fc3baf5d422a6cc5a
Gorka <gorka@aquigorka.com>
2017-11-16 09:01:25 +0100

    Theater: setup for p2p connection

--- src/App/Theater/Connect/index.js
+++ src/App/Theater/Connect/index.js
@@ -1,5 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
+import qr from 'qr-encode'
+import GoogleURL from 'google-url'
 
-const Connect = () => <div>Connect</div>
+const googleUrl = new GoogleURL({ key: GOOGLE_APIKEY })
+
+class Connect extends Component {
+
+  state = { link: null }
+
+  async componentDidMount() {
+    const signal = await this.props.peer.initiator()
+    const url = document.location.origin + '/p'
+    googleUrl.shorten(`${url}?signal=${btoa(JSON.stringify(signal))}`, (err, res) => {
+      if (err) {
+        console.log('Error: ', err)
+      }
+      this.setState({ link: res })
+    })
+  }
+
+  render() {
+    const { link } = this.state
+    if (!link) {
+      return <div>Loading</div>
+    }
+
+    return <div>
+      <div>Follow the QR Code or the link with your smartphone:</div>
+      <div>{qr(link, { type: 6, size: 6, level: 'Q'})}</div>
+      <div>{link}</div>
+    </div>
+  }
+}
 
 export default Connect
--- src/nsa/index.js
+++ src/nsa/index.js
@@ -1,3 +1,38 @@
-const nsa = {}
+import Peer from 'simple-peer'
+import EventEmitter from 'events'
 
-export default nsa
+/*
+return new Promise((resolve, reject) => {
+ 15       this.peer = new Peer({ initiator:true , trickle: false })
+ 16       this.peer.on('signal', data => resolve(data))
+ 17       this.peer.on('connect', () => this.emit('connect'))
+ 18       this.peer.on('data', data => this.emit('data', data))
+ 19     })
+  */
+
+class NSA extends EventEmitter {
+  constructor() {
+    super()
+    this.peer = null
+  }
+
+  initiator() {
+    return new Promise((resolve, reject) => {
+      this.peer = new Peer({ initiator: true, trickle: false })
+      this.peer.on('signal', resolve)
+      this.peer.on('connect', this.emit('connect'))
+      this.peer.on('data', this.emit('data', JSON.parse(data)))
+    })
+  }
+
+  connect(data) {
+    if (!peer) {
+      throw new Error('Please use initiator or sginal')
+    }
+    peer.signal(data)
+  }
+
+  signal(data) {}
+}
+
+export default new NSA()
