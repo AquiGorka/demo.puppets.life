1d84f9ec9c67579d5e81d9b429af283a8cf6d5a1
Gorka <gorka@aquigorka.com>
2017-11-17 08:46:51 +0100

    Connect: add form to connect using id

--- src/App/Theater/Connect/index.js
+++ src/App/Theater/Connect/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import qr from 'qr-encode'
 import GoogleURL from 'google-url'
+import nsa from '../../../nsa'
 
 const googleUrl = new GoogleURL({ key: GOOGLE_APIKEY })
 
@@ -9,7 +10,7 @@ class Connect extends Component {
   state = { link: null }
 
   async componentDidMount() {
-    const signal = await this.props.peer.initiator()
+    const signal = await nsa.initiator()
     const url = (PUPPETEER_URL ? PUPPETEER_URL : document.location.origin) + '/p'
     googleUrl.shorten(`${url}?signal=${btoa(JSON.stringify(signal))}`, (err, res) => {
       if (err) {
@@ -17,6 +18,7 @@ class Connect extends Component {
       }
       this.setState({ link: res })
     })
+    nsa.on('connect', this.props.onConnect)
   }
 
   render() {
@@ -29,7 +31,20 @@ class Connect extends Component {
       <div>Follow the QR Code or the link with your smartphone:</div>
       <div><img src={qr(link, { type: 6, size: 6, level: 'Q'})} /></div>
       <div>{link}</div>
+      <form onSubmit={this.onSubmit} ref={f => (this.form = f)}>
+        <input type="text" />
+      </form>
     </div>
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+    const id = this.form.elements[0].value
+    googleUrl.expand(`https://goo.gl/${id}`, (err, longUrl) => {
+      //console.log(longUrl)
+      const data = JSON.parse(atob(longUrl.split('?signal=')[1]))
+      nsa.connect(data)
+    })
   }
 }
 
