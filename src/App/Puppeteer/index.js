6cfc01acb0f38144d17cb9447157f0677bce4a02
Gorka <gorka@aquigorka.com>
2017-11-17 08:27:22 +0100

    Puppeteer: add receive signal and display id

--- src/App/Puppeteer/index.js
+++ src/App/Puppeteer/index.js
@@ -1,5 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
+import GoogleURL from 'google-url'
+import qmark from 'qmark'
+import nsa from '../../nsa'
 
-const Puppeteer = () => <div>Puppeteer</div>
+const googleUrl = new GoogleURL({ key: GOOGLE_APIKEY })
+const PUPPETEER = 'p2p connected, start sending gyro data'
+const CONNECT = 'waiting for connection'
+const ERROR = 'something went wrong'
+const NOID = 'no signal data was found'
+
+const Controls = () => <div>C</div>
+
+class Puppeteer extends Component {
+
+  state: { mode: null }
+
+  async componentDidMount() {
+    const signal = atob(qmark('signal'))
+    if (signal) {
+      const data = await nsa.signal(signal)
+      googleUrl.shorten(`${location.origin}${location.pathname}?signal=${btoa(JSON.stringify(data))}`, (err, shortUrl) => {
+        // remove the length of https://goo.gl/
+        const id = shortUrl.substr(15)
+        //console.log(id)
+        this.setState({  mode: CONNECT, id  })
+      })
+    } else {
+      this.setState({  mode: NOID, msg: '' })
+    }
+    nsa.on('connect', () => this.setState({ mode: PUPPETEER }))
+  }
+
+  render() {
+    const { mode, id } = this.state
+
+    if (mode === PUPPETEER) {
+      return <Controls />
+    }
+
+    if (mode === CONNECT) {
+      return <div>{id}</div>
+    }
+
+    return <div>Please provide the correct information</div>
+  }
+}
 
 export default Puppeteer
--- src/nsa/index.js
+++ src/nsa/index.js
@@ -7,23 +7,34 @@ class NSA extends EventEmitter {
     this.peer = null
   }
 
+   init = () => {
+    this.peer.on('connect', () => this.emit('connect'))
+    this.peer.on('data', () => this.emit('data', JSON.parse(data)))
+  }
+
   initiator() {
     return new Promise((resolve, reject) => {
       this.peer = new Peer({ initiator: true, trickle: false })
       this.peer.on('signal', resolve)
-      this.peer.on('connect', () => this.emit('connect'))
-      this.peer.on('data', () => this.emit('data', JSON.parse(data)))
+      this.init()
     })
   }
 
   connect(data) {
-    if (!peer) {
-      throw new Error('Please use initiator or sginal')
+    if (!this.peer) {
+      throw new Error('Please use initiator or signal')
     }
-    peer.signal(data)
+    this.peer.signal(data)
   }
 
-  signal(data) {}
+  signal(data) {
+    return new Promise((resolve, reject) => {
+      this.peer = new Peer({ trickle: false })
+      this.peer.signal(data)
+      this.peer.on('signal', resolve)
+      this.init()
+    })
+  }
 }
 
 export default new NSA()
