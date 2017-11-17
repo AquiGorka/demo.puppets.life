a810990728b0ada22deffadf5206d2378084ace8
Gorka <gorka@aquigorka.com>
2017-11-17 08:44:59 +0100

    Theater: remove peer as parameter

--- src/App/Theater/index.js
+++ src/App/Theater/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Connect from './Connect'
 import Simulation from './Simulation'
-import peer from '../../nsa'
 
 const CONNECT = 'provide the user with the url to open on their phone and the input to receive their id'
 const SIMULATION = 'run the 3D simulation'
@@ -12,10 +11,10 @@ class Theater extends Component {
 
   render() {
     if (this.state.mode === CONNECT) {
-      return <Connect peer={peer} onConnect={() => this.setState({ mode: Simulation })} />
+      return <Connect onConnect={() => this.setState({ mode: Simulation })} />
     }
 
-    return <Simulation peer={peer} onDisconnect={() => this.setState({ mode: CONNECT })} />
+    return <Simulation onDisconnect={() => this.setState({ mode: CONNECT })} />
   }
 }
 
